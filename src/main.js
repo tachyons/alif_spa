5e003b01b817a751150bc03580e17a5125c397e7
Aboobacker MK <aboobackervyd@gmail.com>
2020-06-07 06:52:51 +0530

    Configure tailwind css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import './assets/styles/index.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
